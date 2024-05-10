1aef663fe9135bd26995b9fcedf81a514e2a2054
gregoryspecchi <gregoryspecchi@gmail.com>
2024-05-10 09:59:05 +0200

    aggiornamenti

--- app_prenotazione.py
+++ app_prenotazione.py
@@ -9,19 +9,19 @@ import toml
 
 # Configura Firebase Admin SDK
 
-firebase_secrets = toml.load('secrets.toml')
 firebase_credentials = {
-    "type": firebase_secrets["FIREBASE_TYPE"],
-    "project_id": firebase_secrets["FIREBASE_PROJECT_ID"],
-    "private_key_id": firebase_secrets["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": firebase_secrets["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
-    "client_email": firebase_secrets["FIREBASE_CLIENT_EMAIL"],
-    "client_id": firebase_secrets["FIREBASE_CLIENT_ID"],
-    "auth_uri": firebase_secrets["FIREBASE_AUTH_URI"],
-    "token_uri": firebase_secrets["FIREBASE_TOKEN_URI"],
-    "auth_provider_x509_cert_url": firebase_secrets["FIREBASE_AUTH_PROVIDER_CERT_URL"],
-    "client_x509_cert_url": firebase_secrets["FIREBASE_CLIENT_CERT_URL"]
+    "type": st.secrets["FIREBASE_TYPE"],
+    "project_id": st.secrets["FIREBASE_PROJECT_ID"],
+    "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": st.secrets["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
+    "client_id": st.secrets["FIREBASE_CLIENT_ID"],
+    "auth_uri": st.secrets["FIREBASE_AUTH_URI"],
+    "token_uri": st.secrets["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": st.secrets["FIREBASE_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": st.secrets["FIREBASE_CLIENT_CERT_URL"]
 }
+
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_credentials)
