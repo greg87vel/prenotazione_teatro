e1545143eb655192289f9920a1a62d7c265ecdf6
gregoryspecchi <gregoryspecchi@gmail.com>
2024-05-10 11:18:57 +0200

    aggiornamenti

--- app_prenotazione.py
+++ app_prenotazione.py
@@ -26,16 +26,16 @@ st.title("Servizio di prenotazione posti")
 # Configura Firebase Admin SDK
 
 firebase_credentials = {
-    "type": os.environ["FIREBASE_TYPE"],
-    "project_id": os.environ["FIREBASE_PROJECT_ID"],
-    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
-    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": os.environ["FIREBASE_CLIENT_ID"],
-    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
-    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
-    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_CERT_URL"],
-    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_CERT_URL"]
+    "type": os.environ["FB_TYPE"],
+    "project_id": os.environ["FB_PROJECT_ID"],
+    "private_key_id": os.environ["FB_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FB_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["FB_CLIENT_EMAIL"],
+    "client_id": os.environ["FB_CLIENT_ID"],
+    "auth_uri": os.environ["FB_AUTH_URI"],
+    "token_uri": os.environ["FB_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FB_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": os.environ["FB_CLIENT_CERT_URL"]
 }
 
 
