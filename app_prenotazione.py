c2d65703bb72e40cfeebc9beac1e5f101c050100
gregoryspecchi <gregoryspecchi@gmail.com>
2024-05-10 10:45:10 +0200

    aggiornamenti

--- app_prenotazione.py
+++ app_prenotazione.py
@@ -2,6 +2,7 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 import time
+import os
 
 # ----------------------------
 # IMPOSTAZIONI
@@ -25,16 +26,16 @@ st.title("Servizio di prenotazione posti")
 # Configura Firebase Admin SDK
 
 firebase_credentials = {
-    "type": st.secrets.FIREBASE_TYPE,
-    "project_id": st.secrets.FIREBASE_PROJECT_ID,
-    "private_key_id": st.secrets.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": st.secrets.FIREBASE_PRIVATE_KEY.replace('\\n', '\n'),
-    "client_email": st.secrets.FIREBASE_CLIENT_EMAIL,
-    "client_id": st.secrets.FIREBASE_CLIENT_ID,
-    "auth_uri": st.secrets.FIREBASE_AUTH_URI,
-    "token_uri": st.secrets.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": st.secrets.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    "client_x509_cert_url": st.secrets.FIREBASE_CLIENT_CERT_URL
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_CERT_URL"]
 }
 
 
